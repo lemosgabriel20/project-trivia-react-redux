65c32c57a9809b23426e86a8deca1c687ef9145d
Gabriel Lemos <jmjgb@outlook.com>
2023-02-10 17:16:08 -0300

    Criado botão Home na tela de Ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,33 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { clearState } from '../redux/actions';
 
-export default class Ranking extends Component {
+class Ranking extends Component {
+  handleClick = () => {
+    const { history, dispatch } = this.props;
+    history.push('/');
+    dispatch(clearState());
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          onClick={ this.handleClick }
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  dispatch: PropTypes.func,
+  history: PropTypes.any,
+}.isRequired;
+
+export default connect()(Ranking);
